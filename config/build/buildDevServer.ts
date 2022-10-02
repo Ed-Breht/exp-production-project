400eff4a56399b05b66e743fb7de4bfa0dd8101b
Брехт Эдуард Александрович <@ebrekht>
2022-10-02 12:35:45 +0300

    add config

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,10 @@
+import {BuildOptions} from "./types/config";
+import { Configuration as DevServerConfigurations} from "webpack-dev-server"
+
+export function buildDevServer(options: BuildOptions): DevServerConfigurations {
+    return {
+        port: options.port,
+        open: true,
+        historyApiFallback: true
+    }
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,30 @@
+import webpack from "webpack";
+import {BuildOptions} from "./types/config";
+import path from "path";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { mode, paths, isDev} = options
+
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(options)
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? "inline-source-map" : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined
+    }
+
+
+}
