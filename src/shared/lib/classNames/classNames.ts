e7a091851387dfeb2caac4429468eaa1d5b5458f
Брехт Эдуард Александрович <@ebrekht>
2022-10-19 18:13:41 +0300

    add jest and first test

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,15 +1,12 @@
-type Mods = Record<string, boolean | string>;
+type Mods = Record<string, boolean | string>
 
-export function classNames(
-  cls: string,
-  mods?: Mods,
-  additional?: string[]
-): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
   return [
     cls,
     ...additional.filter(Boolean),
     ...Object.entries(mods)
-      .filter(([className, value]) => Boolean(value))
-      .map(([className]) => className),
-  ].join(" ");
+        .filter(([_, value]) => Boolean(value))
+        .map(([className]) => className),
+  ]
+      .join(' ');
 }
