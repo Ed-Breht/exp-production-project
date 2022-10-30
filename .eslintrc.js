8960fb0871bcc27f1d9398fed563b092a9c9c5bb
Брехт Эдуард Александрович <@ebrekht>
2022-10-30 17:43:38 +0300

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,10 +33,21 @@ module.exports = {
     "import/extensions": "off",
     "import/no-extraneous-dependencies": "off",
     "no-underscore-dangle": "off",
-    "i18next/no-literal-string": ["error", { markupOnly: true }],
+    "i18next/no-literal-string": [
+      "error",
+      { markupOnly: true, ignoreAttribute: ["data-testid"] },
+    ],
     "max-len": ["error", { ignoreComments: true, code: 100 }],
   },
   globals: {
     __IS_DEV__: true,
   },
+  overrides: [
+    {
+      files: ["**/src/**/*.test.{ts,tsx}"],
+      rules: {
+        "i18next/no-literal-string": "off",
+      },
+    },
+  ],
 };
