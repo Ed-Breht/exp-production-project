84d795fb0744bbb3db413a70bf9d10dd3d070d42
Брехт Эдуард Александрович <@ebrekht>
2022-12-22 22:42:19 +0300

    add pre commit

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,19 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {useTranslation} from "react-i18next";
-import {Input} from "shared/ui/Input/Input";
 
 const MainPage = () => {
     const {t} = useTranslation()
-    const [state, setState] = useState("");
-
-    const onChange = (value: string) => {
-        setState(value);
-    };
 
     return (
         <div>
             {t('Главная страница')}
-            <Input value={state} placeholder={"Введите текст"} onChange={onChange} />
         </div>
     );
 };
