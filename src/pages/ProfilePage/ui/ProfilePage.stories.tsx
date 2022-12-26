e229e6d86fea59d0518ab34f684efd6071a90cdd
Брехт Эдуард Александрович <@ebrekht>
2022-12-26 23:08:26 +0300

    fix profile stories

---
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {ThemeDecorator} from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import {Theme} from "app/providers/ThemeProvider";
+import ProfilePage from "pages/ProfilePage/ui/ProfilePage";
+import {StoreDecorator} from "shared/config/storybook/StoreDecorator/StoreDecorator";
+
+export default {
+    title: 'pages/ProfilePage',
+    component: ProfilePage,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    }
+} as ComponentMeta<typeof ProfilePage>;
+
+const Template: ComponentStory<typeof ProfilePage> = (args) => <ProfilePage />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
+Normal.decorators = [StoreDecorator({})];
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
