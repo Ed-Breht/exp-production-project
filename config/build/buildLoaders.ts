48fd46a2b4189c567e1e545416c8a5e846a253a4
Брехт Эдуард Александрович <@ebrekht>
2023-01-02 18:37:16 +0300

    add new theme , add react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,23 +1,16 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     };
 
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-            },
-        },
-    };
+    const babelLoader = buildBabelLoader(options);
 
     const cssLoader = buildCssLoader(isDev);
 
---
+++ config/build/loaders/buildBabelLoader.ts
@@ -0,0 +1,17 @@
+import { BuildOptions } from '../types/config';
+
+export function buildBabelLoader({ isDev }: BuildOptions) {
+    return {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    isDev && require.resolve('react-refresh/babel'),
+                ].filter(Boolean),
+            },
+        },
+    };
+}
