81576605fa6417bf258b9de8a66c3069df385a68
Брехт Эдуард Александрович <@ebrekht>
2023-01-08 16:28:27 +0300

    fix sidebar

---
+++ src/widgets/Sidebar/model/selectors/getSidebarItems.ts
@@ -0,0 +1,45 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { getUserAuthData } from 'entities/User';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import MainIcon from 'shared/assets/icons/Vector.svg';
+import AboutIcon from 'shared/assets/icons/clarity_list-outline-badged.svg';
+import ProfileIcon from 'shared/assets/icons/Profile.svg';
+import ArticlesIcon from 'shared/assets/icons/Articles.svg';
+import { SidebarItemType } from '../types/sidebar';
+
+export const getSidebarItems = createSelector(
+    getUserAuthData,
+    (userData) => {
+        const sidebarItemsList: SidebarItemType[] = [
+            {
+                path: RoutePath.main,
+                Icon: MainIcon,
+                text: 'Главная',
+            },
+            {
+                path: RoutePath.about,
+                Icon: AboutIcon,
+                text: 'О сайте',
+            },
+        ];
+
+        if (userData) {
+            sidebarItemsList.push(
+                {
+                    path: RoutePath.profile + userData.id,
+                    Icon: ProfileIcon,
+                    text: 'Профиль',
+                    authOnly: true,
+                },
+                {
+                    path: RoutePath.articles,
+                    Icon: ArticlesIcon,
+                    text: 'Статьи',
+                    authOnly: true,
+                },
+            );
+        }
+
+        return sidebarItemsList;
+    },
+);
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,9 +3,10 @@ import { memo, useMemo, useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
+import { useSelector } from 'react-redux';
 import cls from './Sidebar.module.scss';
-import { SidebarItemsList } from '../../model/items';
 import { SidebarItem } from '../SidebarItem/SidebarItem';
+import { getSidebarItems } from '../../model/selectors/getSidebarItems';
 
 interface SidebarProps {
     className?: string;
@@ -13,18 +14,19 @@ interface SidebarProps {
 
 export const Sidebar = memo(({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const sidebarItemsList = useSelector(getSidebarItems);
 
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
 
-    const itemsList = useMemo(() => SidebarItemsList.map((item) => (
+    const itemsList = useMemo(() => sidebarItemsList.map((item) => (
         <SidebarItem
             item={item}
             collapsed={collapsed}
             key={item.path}
         />
-    )), [collapsed]);
+    )), [collapsed, sidebarItemsList]);
 
     return (
         <div
