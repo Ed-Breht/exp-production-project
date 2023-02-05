380e89affa5f23bb18f412af647d74fe2148f7f3
Брехт Эдуард Александрович <@ebrekht>
2023-02-05 16:49:54 +0300

    add html reporter

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -41,6 +41,15 @@ export default {
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
     transformIgnorePatterns: ['node_modules/(?!axios)'],
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+            inlineSource: true,
+        }],
+    ],
 
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
