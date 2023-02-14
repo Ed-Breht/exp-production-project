cbe36335723659ac4ca2f8d6a5667815f3e18a01
Брехт Эдуард Александрович <@ebrekht>
2023-02-14 10:50:57 +0300

    update custom eslint plugin, add support for alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -58,7 +58,7 @@ module.exports = {
         'no-param-reassign': 'off',
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
-        'ed-product-plugin/path-checker': 'error',
+        'ed-product-plugin/path-checker': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
