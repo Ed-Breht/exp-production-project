ed8afea1f94537be5630f0e365f1a65a10f02b98
Брехт Эдуард Александрович <@ebrekht>
2023-02-24 20:30:20 +0300

    add e2e tests

---
+++ cypress/support/commands.ts
@@ -0,0 +1,11 @@
+import { login } from './commands/login';
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      login(email?: string, password?: string): Chainable<void>
+    }
+  }
+}
+
+Cypress.Commands.add('login', login);
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -37,7 +37,11 @@ const ArticlesPage = (props: ArticlesPageProps) => {
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount={false}>
-            <Page onScrollEnd={onLoadNextPart} className={classNames(cls.ArticlesPage, {}, [className])}>
+            <Page
+                data-testid="ArticlesPage"
+                onScrollEnd={onLoadNextPart}
+                className={classNames(cls.ArticlesPage, {}, [className])}
+            >
                 <ArticlesPageFilters />
                 <ArticleInfiniteList className={cls.list} />
             </Page>
