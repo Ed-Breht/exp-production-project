d4f75603453e88379ae8c624e9f260f6201a794c
Брехт Эдуард Александрович <@ebrekht>
2023-03-01 16:52:51 +0300

    add authorization info

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -7,6 +7,9 @@ const AboutPage = memo(() => {
     return (
         <Page>
             <div data-testid="AboutPage">{t('О сайте')}</div>
+            <div data-testid="AboutPage">
+                {t('Авторизоваться можно по логину admin и паролю 123')}
+            </div>
         </Page>
     );
 });
