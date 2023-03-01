c507618f782f27cc4796366db82d2a35d85a73f9
Брехт Эдуард Александрович <@ebrekht>
2023-03-01 17:00:22 +0300

    fix

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -7,9 +7,7 @@ const AboutPage = memo(() => {
     return (
         <Page>
             <div data-testid="AboutPage">{t('О сайте')}</div>
-            <div data-testid="AboutPage">
-                {t('Авторизоваться можно по логину admin и паролю 123')}
-            </div>
+            <div>{t('Авторизоваться можно по логину admin и паролю 123')}</div>
         </Page>
     );
 });
